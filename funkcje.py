9d7c48febc8d614143c6020e7e353f6636d98acb
marcin <morgas.marcin@gmail.com>
2024-01-30 00:06:25 +0100

    dodanie obslugi plikow lokalnych

---
+++ funkcje.py
@@ -0,0 +1,22 @@
+def menu_naglowki():
+    print(40*'*')
+    print('''Dostępne akcje dla pliku:
+    1: Wydrukuj zestaw danych
+    2:
+    3:
+    4:
+    5:
+    ''')
+    print(40*'*')
+def menu_bez_naglowkow():
+    print(40*'*')
+    print('''Dostępne akcje dla pliku:
+    1: Wydrukuj zestaw danych
+    2:
+    3:
+    4:
+    ''')
+    print(40*'*')
+
+def kontunuacja():
+    input('Kliknij enter aby kontynuować...')
--- main.py
+++ main.py
@@ -1,29 +1,52 @@
-# with open("iris.data", 'r') as file:
-#     dataset = []
-#     for line in file:
-#         lista = line.split(',')
-#         lista[-1] = lista[-1].replace('\n', '')
-#         # print(lista)
-#         # break
-#         dataset.append(lista)
-#
-# def ciecie_listy(lista, znacznik1 = None, znacznik2 = None):
-#     print(lista[znacznik1:znacznik2])
-#
-# # print(dataset)
-# # [print(i) for i in dataset]
-#
-# print(dataset[1][1].upper())
-
 import os
-def menu_load_dataset():
-    print('''WCZYTANIE DANYCH
-    1 - Pliki z folderu /bazy do wczytania
-    2 - Ścieżka do pliku
-    3 - Link
-    9 - Powrót do menu
-    0 - Zakończenie programu''')
+import csv
+import time
+import sys
+from funkcje import *
 
+def local_file():
+    rozszerzenia = ['.xlsx', '.data', '.csv', '.xls']
+    os.system('cls')
+    lista_plikow_all = os.listdir()
+    lista_datasetow = [el for el in lista_plikow_all if el[el.find('.'):] in rozszerzenia]
+    if len(lista_datasetow) > 0:
+        print('LISTA PLIKÓW')
+    else:
+        print(f'Brak plików lokalnych z rozszerzeniem {rozszerzenia}')
+        return 0
+    i = 1
+    while i <= len(lista_datasetow):
+        print(f'{i}: {lista_datasetow[i - 1]}')
+        if i == len(lista_datasetow):
+            print('0: Powrót do poprzedniego menu')
+        i += 1
+    while True:
+        try:
+            choice_2 = int(input('Wybierz dateset: '))
+            break
+        except:
+            print('Wprowadzono niepoprawną wartość!')
+    if choice_2 != 0:
+        lp_data_set = choice_2 - 1
+    else:
+        return 0
+    while True:
+        czy_naglowki = input('Czy zestaw zawiera nagłówki? T/n: ').lower()
+        if czy_naglowki == 't' or czy_naglowki == 'n':
+            break
+        else:
+            print('Wprowadzono niepoprawną wartość.')
+    while True:
+        separator = input('Podaj separator (domyślnie to ";"): ')
+        # print(f'sep = {separator}\nlen sep = {len(separator)}')
+        if len(separator) == 1:
+            break
+        elif len(separator) == 0:
+            separator = ';'
+            break
+        else:
+            print('Wygląda na to, że wprowadziłeś więcej niż jeden separator!')
+    load_dataset(lista_datasetow[lp_data_set], czy_naglowki, separator)
 
 
 def main(filepath):
@@ -31,7 +54,8 @@ def main(filepath):
     while True:
         print('*'*40)
         print('''MENU GŁÓWNE
-        1 - Wczytanie data setu
+        1 - Wczytanie data setu z listy plików z folderu projektu
+        2 - Wczytanie data setu z wskazanej ścieżki
         0 - Zakończenie programu''')
         print('*'*40)
         while True:
@@ -43,30 +67,48 @@ def main(filepath):
 
         match choice:
             case 1:
-                load_dataset()
+                local_file()
+            case 2:
+                pass
             case 0:
                 exit('Do zobaczenia!', )
 
-def load_dataset():
-    os.system('cls')
-    menu_load_dataset()
+def load_dataset(sciezka, naglowki, separator):
     try:
-        choice_2 = int(input('Wybierz opcję: '))
+        with open(sciezka, 'r') as file:
+            reader = csv.reader(file, delimiter=separator)
+            dane = []
+            if naglowki == 't':
+                for row in reader:
+                    lista_naglowki = row
+                    break
+                for row in reader:
+                    dane.append(row)
+            else:
+                for row in reader:
+                    dane.append(row)
+        if len(dane[len(dane)-1]) == 0:
+            dane = dane[:-1]
     except:
-        print('Wprowadzono niepoprawną wartość!')
-
-    match choice_2:
-        case 1:
-            print(os.listdir())
-
-        case 2:
-            pass
-        case 3:
-            pass
-        case 9:
-            next
-        case 0:
-            exit('Do zobaczenia')
+        print(f'Błąd przy wczytywaniu danych {sys.exc_info()}. Powrót do menu głównego...')
+        time.sleep(3)
+        return 0
+    while True:
+        if naglowki == 't':
+            menu_naglowki()
+        else:
+            menu_bez_naglowkow()
+        try:
+            choice_2 = int(input('Jaką czynność wybierasz?: '))
+        except:
+            print('Podano nieprawidłową wartość')
+        match choice_2:
+            case 1:
+                print(dane)
+                kontunuacja()
+            case 0:
+                return 0
+    match
 
 if __name__ == "__main__":
     filepath = ''
